ce3cdbe835ebd52c9c74f397fdb690f5c865052a
Luciscb <lucianasouzacastelo04@gmail.com>
2025-03-13 23:05:10 +0000

    desafio xadrez nível novato

--- xadrez.c
+++ xadrez.c
@@ -1,32 +1,28 @@
 #include <stdio.h>
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
-
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    int t, b, r;
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
-
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
+    //MOVIMENTAÇÃO TORRE:
+    for (t = 0; t < 5; t++)
+    {
+        printf("Direita\n", t);
+    }
+    
+    //MOVIMENTAÇÃO BISPO
+    do
+    {
+        printf("Cima, Direita\n", b);
+        b++;
+    } while (b < 5);
+    
+    //MOVIMENTAÇÃO RAINHA
+    while (r < 8)
+    {
+        printf("Esquerda\n", r);
+        r++;
+    }
+    
 
     return 0;
 }
