71cf85256cdba79a63fd0ead7a9162e93d7bd272
Luciscb <lucianasouzacastelo04@gmail.com>
2025-03-17 13:59:58 +0000

    DESAFIO XADREZ NÍVEL MESTRE

--- xadrez.c
+++ xadrez.c
@@ -1,37 +1,48 @@
 #include <stdio.h>
 
-int main() {
-    int t, b = 1, r = 1, c = 1;
-
-    //MOVIMENTAÇÃO TORRE:
-    for (t = 0; t < 5; t++)
-    {
-        printf("Direita\n", t);
+//MOVIMENTAÇÃO TORRE
+void movertorre (int casas1){
+    if (casas1 > 0) {
+        printf("Direita\n");
+        movertorre (casas1 - 1);
     }
-    
-    //MOVIMENTAÇÃO BISPO
-    do
-    {
-        printf("Cima, Direita\n", b);
-        b++;
-    } while (b <= 5);
-    
-    //MOVIMENTAÇÃO RAINHA
-    while (r <= 8)
-    {
-        printf("Esquerda\n", r);
-        r++;
-    }
-    
-    while (c--)
-    {
-       for (int i = 0; i < 2; i++)
-       {
-        printf("Cima\n");
-       }
-       printf("Direita\n");
-    }
-    
-
-    return 0;
 }
+
+//MOVIMENTAÇÃO BISPO
+void moverbispo (int casas2){
+    if (casas2 > 0) {
+        printf("Cima\n");
+        printf("Esquerda\n");
+        moverbispo (casas2 - 1);
+    }
+}
+
+//MOVIMENTAÇÃO RAINHA
+void moverrainha (int casas3){
+    if (casas3 > 0) {
+        printf("Esquerda\n");
+        moverrainha (casas3 - 1);
+    }
+}
+
+//MOVIMENTAÇÃO CAVALO
+void movercavalo (int casas4){
+    if (casas4 > 0) {
+        printf("Cima\n");
+        movercavalo (casas4 - 1);
+    }
+   
+}
+
+
+int main() {
+    movertorre (5);
+    moverbispo (5);
+    moverrainha (8);
+    movercavalo (2);
+    printf("Direita\n");
+}
+
+
+    
+    
